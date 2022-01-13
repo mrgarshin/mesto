e2700b78702911cb3d8aa30e3f9922835a14c0ec
mrgarshin <mrgarshin@yandex.ru>
2022-01-13 16:37:51 +0300

    feat: реализовать кнопку сохранения

--- script.js
+++ script.js
@@ -4,16 +4,23 @@ let popUpCloseButton = document.querySelector('.popup__close-button');
 let popUpSaveButton = document.querySelector('.popup__save-button');
 let userName = document.querySelector('.user__name');
 let userDescription = document.querySelector('.user__description');
-
-
-
+let nameInput = document.querySelector('.popup__input_name')
+let jobInput = document.querySelector('.popup__input_job')
 
 function openPopUp() {
+  nameInput.value = userName.textContent;
+  jobInput.value = userDescription.textContent;
   popUp.classList.add('popup__opened');
 }
 function closePopUp() {
   popUp.classList.remove('popup__opened');
 }
+function saveChanges() {
+  userName.textContent = nameInput.value;
+  userDescription.textContent = jobInput.value;
+  popUp.classList.remove('popup__opened');
+}
 
 profileEditButton.addEventListener('click', openPopUp);
 popUpCloseButton.addEventListener('click', closePopUp);
+popUpSaveButton.addEventListener('click', saveChanges);
